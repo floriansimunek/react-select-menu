e837c9e4c25c56ed925f591efdecf1e1cf935cc3
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-10 01:59:22 +0200

    :label: Add types for Label component

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -1,4 +1,6 @@
-const Label = ({ children }) => {
+import { LabelProps } from '@types';
+
+const Label: React.FC<LabelProps> = ({ children }) => {
   return <label htmlFor="">{children}</label>;
 };
 
