83d8ac5beffaf12b068537f30af77c3d0fdd4f06
Florian SIMUNEK <60973568+floriansimunek@users.noreply.github.com>
2023-09-10 18:40:58 +0200

    :sparkles: Add handleClickOutside (#22)

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -3,12 +3,24 @@ import Item from '@components/Item';
 import Label from '@components/Label';
 import Menu from '@components/Menu';
 import { SelectProps } from '@types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './index.scss';
 
 const Select: React.FC<SelectProps> = ({ options }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [selected, setSelected] = useState<string | undefined>(undefined);
+  const [selected, setSelected] = useState<string>('');
+
+  useEffect(() => {
+    const handleClickOutside = (e: Event) => {
+      const target = e.target as HTMLElement;
+      if (isOpen && !target.closest('.select__menu')) {
+        setIsOpen(false);
+      }
+    };
+    isOpen && document.addEventListener('click', handleClickOutside);
+
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, [isOpen]);
 
   return (
     <div className={`select__menu`} data-rsm-is-open={isOpen}>
