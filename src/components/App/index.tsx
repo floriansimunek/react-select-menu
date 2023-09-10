e56f0b8483c63d85e8c3fb6998997639afdc96cc
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-10 02:15:49 +0200

    :sparkles: Call Select component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,13 @@
+import Select from '@components/Select';
+
 function App() {
-  return <></>;
+  const options = [
+    { value: 'Option 0' },
+    { value: 'Option 1' },
+    { value: 'Option 2' },
+  ];
+
+  return <Select options={options} />;
 }
 
 export default App;
