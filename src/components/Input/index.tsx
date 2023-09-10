be7623ed79c90f2a6f178528a73e7d1f6566d268
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-10 15:56:26 +0200

    :sparkles: Add handleClick on input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,19 @@
 import { InputProps } from '@types';
 
 const Input: React.FC<InputProps> = ({ placeholder }) => {
-  return <input type="text" placeholder={placeholder} />;
+  const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    const input = e.target as HTMLInputElement;
+    const selectMenu = input.closest('.select__menu');
+    selectMenu?.classList.toggle('open');
+  };
+
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      onClick={(e) => handleClick(e)}
+    />
+  );
 };
 
 export default Input;
