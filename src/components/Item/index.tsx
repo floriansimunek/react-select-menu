5241fd09554ea768f21fa1092914f85497912be1
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-10 16:32:00 +0200

    :lipstick: Add Item styles

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,7 +1,12 @@
 import { ItemProps } from '@types';
+import './index.scss';
 
 const Item: React.FC<ItemProps> = ({ children, value }) => {
-  return <li data-value={value}>{children}</li>;
+  return (
+    <li className="select__menu--item" data-value={value}>
+      {children}
+    </li>
+  );
 };
 
 export default Item;
