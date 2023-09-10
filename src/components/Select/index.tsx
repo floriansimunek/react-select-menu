3c6d7b9869ae19eb44ed11bbf979e57491789ba4
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-10 02:15:11 +0200

    :label: Add types for Select component

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -2,8 +2,9 @@ import Label from '@components/Label';
 import Input from '@components/Input';
 import List from '@components/List';
 import Item from '@components/Item';
+import { SelectProps } from '@types';
 
-const Select = ({ options }) => {
+const Select: React.FC<SelectProps> = ({ options }) => {
   return (
     <div>
       <Label>Label</Label>
