7d3548acd7fd5df1e8d7c42bf7be416bf3f25a4d
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-13 02:07:04 +0200

    ✅ Add option group test

--- src/__test__/Select.test.tsx
+++ src/__test__/Select.test.tsx
@@ -11,6 +11,24 @@ describe('SelectMenu', () => {
         { value: 'Option 0' },
         { value: 'Option 1' },
         { value: 'Option 2' },
+        {
+          label: 'Group 1',
+          options: [
+            { value: 'Option 3' },
+            { value: 'Option 4' },
+            { value: 'Option 5' },
+          ],
+        },
+        {
+          label: 'Group 2',
+          options: [
+            { value: 'Option 6' },
+            { value: 'Option 7' },
+            { value: 'Option 8' },
+            { value: 'Option 9' },
+            { value: 'Option 10' },
+          ],
+        },
       ];
       render(<Select options={options} />);
     });
@@ -37,11 +55,14 @@ describe('SelectMenu', () => {
 
     it('should render item', () => {
       const items = screen.getAllByTestId('select__menu--item');
-      expect(items.length).toBe(3);
+      expect(items.length).toBe(11);
 
       items.map((item) => {
         expect(item).toBeInTheDocument();
       });
+
+      const groupLabels = screen.getAllByTestId('select__menu--group-label');
+      expect(groupLabels.length).toBe(2);
     });
   });
 });
--- src/components/GroupLabel/index.tsx
+++ src/components/GroupLabel/index.tsx
@@ -3,7 +3,10 @@ import './index.scss';
 
 const GroupLabel: React.FC<GLabelProps> = ({ option }) => {
   return (
-    <li className="select__menu--group-label">
+    <li
+      className="select__menu--group-label"
+      data-testid="select__menu--group-label"
+    >
       <span>{option.label}</span>
       <span>{option.options?.length}</span>
     </li>
