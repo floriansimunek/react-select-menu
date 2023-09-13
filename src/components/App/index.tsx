15a15bf41ea068239134c3a58ed82a76892dcae5
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-13 02:03:19 +0200

    :sparkles: Add options data

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -5,9 +5,27 @@ function App() {
     { value: 'Option 0' },
     { value: 'Option 1' },
     { value: 'Option 2' },
+    {
+      label: 'Group 1',
+      options: [
+        { value: 'Option 3' },
+        { value: 'Option 4' },
+        { value: 'Option 5' },
+      ],
+    },
+    {
+      label: 'Group 2',
+      options: [
+        { value: 'Option 6' },
+        { value: 'Option 7' },
+        { value: 'Option 8' },
+        { value: 'Option 9' },
+        { value: 'Option 10' },
+      ],
+    },
   ];
 
-  return <Select options={options} />;
+  return <Select options={options} placeholder="Placeholder" />;
 }
 
 export default App;
