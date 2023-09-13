8f120e66f93d1cfab5c52176c88ccc56a08f67be
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-13 02:38:56 +0200

    :sparkles: Add id to label & input

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -1,11 +1,11 @@
 import { LabelProps } from '@types';
 
-const Label: React.FC<LabelProps> = ({ children }) => {
+const Label: React.FC<LabelProps> = ({ children, htmlFor }) => {
   return (
     <label
       className="select__menu--label"
       data-testid="select__menu--label"
-      htmlFor=""
+      htmlFor={htmlFor}
     >
       {children}
     </label>
