ad694c20ee1f94b1e6e0b7a336a1e01ef09d9d2c
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-16 10:42:31 +0200

    ✨ Add index.ts file to export file of the lib

---
+++ src/components/index.ts
@@ -0,0 +1,29 @@
+/* TYPES */
+export type {
+  CloseIconProps,
+  GLabelProps,
+  Group,
+  InputProps,
+  ItemProps,
+  LabelProps,
+  MenuProps,
+  Option,
+  SelectProps,
+} from '@types';
+
+/* COMPONENTS */
+export { default as chevronIcon } from '@components/ChevronIcon';
+export { default as CloseIcon } from '@components/CloseIcon';
+export { default as GroupLabel } from '@components/GroupLabel';
+export { default as Input } from '@components/Input';
+export { default as Item } from '@components/Item';
+export { default as Label } from '@components/Label';
+export { default as Menu } from '@components/Menu';
+export { default as Select } from '@components/Select';
+
+/* STYLES */
+import '@components/GroupLabel/index.scss';
+import '@components/Input/index.scss';
+import '@components/Item/index.scss';
+import '@components/Menu/index.scss';
+import '@components/Select/index.scss';
