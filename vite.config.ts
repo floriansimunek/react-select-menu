4df236d47d05c45ea5b61468c4bee79422dbcaab
Florian SIMUNEK <60973568+floriansimunek@users.noreply.github.com>
2023-09-16 10:43:36 +0200

    ✨ Feature#95 publish to npm (#96)
    * ➕ Add react & react-dom to devDependencies
    
    * 🔧 Add config for npm publishing
    
    * ✨ Add index.ts file to export file of the lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,32 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import * as packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: ['src/component/'],
+    }),
+  ],
   resolve: {
     alias: {
       '@components': '/src/components',
       '@types': '/src/types/index.d.ts',
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve('src', 'components/index.ts'),
+      name: 'React-SelectMenu',
+      formats: ['es', 'umd'],
+      fileName: (format) => `react-selectmenu.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.dependencies)],
     },
   },
 });
