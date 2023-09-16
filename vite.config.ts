bd53300be862c6c51c3fe18d67beb26a5b8e7421
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-16 21:57:30 +0200

    🔧 Add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,36 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    cssInjectedByJsPlugin(),
+    dts({
+      include: ['src/components/'],
+    }),
+  ],
   resolve: {
     alias: {
       '@components': '/src/components',
       '@types': '/src/types/index.d.ts',
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve('src', 'components/index.ts'),
+      name: 'React-Select-Menu',
+      formats: ['es', 'umd'],
+      fileName: (format) => `react-select-menu.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: { globals: { react: 'React' } },
     },
   },
 });
