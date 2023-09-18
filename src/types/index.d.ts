ea25670ad0f4088eefcb695488de1f130542fa13
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-18 11:25:35 +0200

    🏷️ Add types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -19,6 +19,7 @@ export type InputProps = {
   style?: CSSProperties;
   isDisabled?: boolean;
   isSearchable?: boolean;
+  isRequired?: boolean;
 };
 
 export type LabelProps = {
@@ -49,6 +50,7 @@ export type SelectProps = {
   isClearable?: boolean;
   isSearchable?: boolean;
   isForcedOpen?: boolean;
+  isRequired?: boolean;
   onChange?: () => void;
   onClose?: () => void;
   onCreate?: () => void;
