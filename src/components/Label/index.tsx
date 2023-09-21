f71e9ba126f31c9fb041e8ccea9e31f90545f19a
Florian SIMUNEK <60973568+floriansimunek@users.noreply.github.com>
2023-09-21 16:13:15 +0200

    ✨ Feature#147 add stylelabel (#151)
    * 🏷️ Add types
    
    * ✨ Add styles.label

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -1,11 +1,12 @@
 import { LabelProps } from '@types';
 
-const Label: React.FC<LabelProps> = ({ children, htmlFor }) => {
+const Label: React.FC<LabelProps> = ({ children, htmlFor, style }) => {
   return (
     <label
       className="select__menu--label"
       data-testid="select__menu--label"
       htmlFor={htmlFor}
+      style={style}
     >
       {children}
     </label>
