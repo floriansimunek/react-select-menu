8ff709620bfdb9be83ac741a23e8a80ddb28b7ee
Florian SIMUNEK <florian.simunek@gmail.com>
2023-10-03 16:32:02 +0200

    📝 Add JSDoc for Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,18 @@
 import { InputProps } from '@types';
 import './index.scss';
 
+/**
+ * Input component - used for search option or get select-menu value
+ * @param id Id prop - needed to connect label to the input
+ * @param value value of the input
+ * @param placeholder placeholder of the input
+ * @param onChange onChange event handler
+ * @param isDisabled disabling the input or not
+ * @param isSearchable whether the input is searchable
+ * @param isRequired whether the input is required
+ * @component
+ * @returns {JSX.Element}
+ */
 const Input: React.FC<InputProps> = ({
   id,
   value,
